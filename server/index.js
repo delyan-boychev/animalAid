b54f4908fd8a32a3fdeb71b15dd59ecc619b3ddb
Delyan Boychev <delyan.boychev05@gmail.com>
2021-07-02 15:07:42 +0300

    Connection to database created

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 4000
-
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = 4000;
+mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+  res.send('Hello World!');
+});
 app.listen(port, () => {
   console.log(`Animal Aid server is running!`);
-})
+});
