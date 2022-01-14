48252fb93818e0828c5065d2ab44a2be60568c7a
Delyan Boychev <delyan.boychev05@gmail.com>
2022-01-15 01:34:32 +0200

    Fixed ssl and urls

--- server/index.js
+++ server/index.js
@@ -2,21 +2,31 @@ const express = require("express");
 const config = require("./config.json");
 const mongoose = require("mongoose");
 const app = express();
+const fs = require("fs");
 var cors = require("cors");
+const privateKey = fs.readFileSync("./ssl/key.pem", "utf8");
+const certificate = fs.readFileSync("./ssl/cert.pem", "utf8");
 const updateCaptchaEncryption = require("./captcha/updateCaptchaEncryption");
-const port = 4000;
+const port = 8443;
 const userRoute = require("./routes/user");
 const captchaRoute = require("./routes/captcha");
 const adminRoute = require("./routes/admin");
-const httpServer = require("http").Server(app);
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: "delyan050710!@#$%^&*()",
+};
+const httpServer = require("https").createServer(credentials, app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://192.168.1.102:3000", "http://localhost:3000"],
+    origin: [
+      "http://animalaidbg.com:8080",
+      "https://animalaidbg.com",
+      "http://animalaidbg.com",
+    ],
   },
 });
 const onConnection = require("./chatSockets")(io);
-const helmet = require("helmet");
-app.use(helmet());
 io.on("connection", onConnection);
 app.disable("x-powered-by");
 app.use(express.json({ limit: "10mb" }));
