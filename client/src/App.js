a4d08d59fbc945c0e5d6e2c1bbc7b2f248f05e1e
Delyan Boychev <delyan.boychev05@gmail.com>
2022-01-29 20:39:57 +0200

    Cookies consent added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 function App() {
   return (
     <Router>
@@ -9,6 +10,15 @@ function App() {
       <div>
         <Routes></Routes>
       </div>
+      <CookieConsent
+        disableStyles={true}
+        buttonClasses="btn btn-primary"
+        buttonText="Съгласявам се"
+        containerClasses="alert alert-primary fixed-bottom d-flex justify-content-between mb-0"
+        contentClasses="col"
+      >
+        <p>Този сайт използва бисквитки.</p>
+      </CookieConsent>
     </Router>
   );
 }
