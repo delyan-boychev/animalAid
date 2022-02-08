517d4a170da25f11d9b90e22248d73891cbb66e3
Delyan Boychev <delyan.boychev05@gmail.com>
2022-02-08 16:11:36 +0200

    Fixed vet load

--- client/src/pages/UserPages/Vet.js
+++ client/src/pages/UserPages/Vet.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListGroup, Button } from "react-bootstrap";
+import { ListGroup, Button, Spinner } from "react-bootstrap";
 import {
   faAt,
   faPhoneSquareAlt,
@@ -90,113 +90,121 @@ class Vet extends React.Component {
             this.props.navigate(`/user/chats?chatId=${this.state.vet._id}`)
           }
         ></DialogModal>
-        <h3 className="text-center">
-          {this.state.vet.name.first} {this.state.vet.name.last}
-        </h3>
-        <div className="d-flex justify-content-center mb-3">
-          <img
-            className="mb-3 rounded-circle"
-            src={
-              this.state.vet.imgFileName !== ""
-                ? `${API_URL}/user/img/${this.state.vet.imgFileName}`
-                : ""
-            }
-            height="150px"
-            width="150px"
-            alt="profilePicture"
-          />
+        <div className="text-center" hidden={this.state.vet.name.first !== ""}>
+          <Spinner animation="border" variant="primary" role="status"></Spinner>
         </div>
-        <div className="d-flex flex-row mb-3">
-          <Button
-            onClick={() =>
-              this.openModal(
-                `Сигурни ли сте, че искате да започнете чат с ${this.state.vet.name.first} ${this.state.vet.name.last}?`
-              )
-            }
-          >
-            <FontAwesomeIcon icon={faCommentMedical}></FontAwesomeIcon> Започни
-            чат
-          </Button>
-          <Button
-            onClick={() => window.open(`tel:${this.state.vet.phoneNumber}`)}
-            className="ms-3"
-          >
-            <FontAwesomeIcon icon={faPhoneAlt}></FontAwesomeIcon> Обади се
-          </Button>
+        <div hidden={this.state.vet.name.first === ""}>
+          <h3 className="text-center">
+            {this.state.vet.name.first} {this.state.vet.name.last}
+          </h3>
+          <div className="d-flex justify-content-center mb-3">
+            <img
+              className="mb-3 rounded-circle"
+              src={
+                this.state.vet.imgFileName !== ""
+                  ? `${API_URL}/user/img/${this.state.vet.imgFileName}`
+                  : ""
+              }
+              height="150px"
+              width="150px"
+              alt="profilePicture"
+            />
+          </div>
+          <div className="d-flex flex-row mb-3">
+            <Button
+              onClick={() =>
+                this.openModal(
+                  `Сигурни ли сте, че искате да започнете чат с ${this.state.vet.name.first} ${this.state.vet.name.last}?`
+                )
+              }
+            >
+              <FontAwesomeIcon icon={faCommentMedical}></FontAwesomeIcon>{" "}
+              Започни чат
+            </Button>
+            <Button
+              onClick={() => window.open(`tel:${this.state.vet.phoneNumber}`)}
+              className="ms-3"
+            >
+              <FontAwesomeIcon icon={faPhoneAlt}></FontAwesomeIcon> Обади се
+            </Button>
+          </div>
+          <ListGroup>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon> Описание
+                на ветеринарния лекар:{" "}
+                <span className="fw-normal text-break">
+                  {this.state.vet.vetDescription}
+                </span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faAt}></FontAwesomeIcon> Имейл адрес:{" "}
+                <span className="fw-normal">
+                  <a
+                    href={`mailto:${this.state.vet.email}`}
+                    className="link-primary"
+                  >
+                    {this.state.vet.email}
+                  </a>
+                </span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faPhoneSquareAlt}></FontAwesomeIcon>{" "}
+                Телефонен номер:{" "}
+                <span className="fw-normal">
+                  <a
+                    href={`tel:${this.state.vet.phoneNumber}`}
+                    className="link-primary"
+                  >
+                    {this.state.vet.phoneNumber}
+                  </a>
+                </span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faPaw}></FontAwesomeIcon> Животни, с
+                които ветеринарният лекар практикува:{" "}
+                <span className="fw-normal">
+                  {this.state.vet.typeAnimals.map(
+                    (animal, index) =>
+                      `${animalsTranslate[animal]}${
+                        this.state.vet.typeAnimals.length - 1 > index
+                          ? ", "
+                          : ""
+                      }`
+                  )}
+                </span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faCity}></FontAwesomeIcon> Населено
+                място:{" "}
+                <span className="fw-normal">
+                  {this.state.vet.city.type} {this.state.vet.city.name},{" "}
+                  {this.state.vet.city.region}
+                </span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faMapMarkedAlt}></FontAwesomeIcon> Адрес:{" "}
+                <span className="fw-normal">{this.state.vet.address}</span>
+              </span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="fw-bold">
+                <FontAwesomeIcon icon={faUniversity}></FontAwesomeIcon> УРН:{" "}
+                <span className="fw-normal">{this.state.vet.URN}</span>
+              </span>
+            </ListGroup.Item>
+          </ListGroup>
         </div>
-        <ListGroup>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon> Описание
-              на ветеринарния лекар:{" "}
-              <span className="fw-normal text-break">
-                {this.state.vet.vetDescription}
-              </span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faAt}></FontAwesomeIcon> Имейл адрес:{" "}
-              <span className="fw-normal">
-                <a
-                  href={`mailto:${this.state.vet.email}`}
-                  className="link-primary"
-                >
-                  {this.state.vet.email}
-                </a>
-              </span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faPhoneSquareAlt}></FontAwesomeIcon>{" "}
-              Телефонен номер:{" "}
-              <span className="fw-normal">
-                <a
-                  href={`tel:${this.state.vet.phoneNumber}`}
-                  className="link-primary"
-                >
-                  {this.state.vet.phoneNumber}
-                </a>
-              </span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faPaw}></FontAwesomeIcon> Животни, с които
-              ветеринарният лекар практикува:{" "}
-              <span className="fw-normal">
-                {this.state.vet.typeAnimals.map(
-                  (animal, index) =>
-                    `${animalsTranslate[animal]}${
-                      this.state.vet.typeAnimals.length - 1 > index ? ", " : ""
-                    }`
-                )}
-              </span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faCity}></FontAwesomeIcon> Населено място:{" "}
-              <span className="fw-normal">
-                {this.state.vet.city.type} {this.state.vet.city.name},{" "}
-                {this.state.vet.city.region}
-              </span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faMapMarkedAlt}></FontAwesomeIcon> Адрес:{" "}
-              <span className="fw-normal">{this.state.vet.address}</span>
-            </span>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <span className="fw-bold">
-              <FontAwesomeIcon icon={faUniversity}></FontAwesomeIcon> УРН:{" "}
-              <span className="fw-normal">{this.state.vet.URN}</span>
-            </span>
-          </ListGroup.Item>
-        </ListGroup>
       </div>
     );
   }
