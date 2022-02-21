ec75dc6b8813e57624a0a2697f5c9d6304f735a8
Delyan Boychev <delyan.boychev05@gmail.com>
2022-02-21 17:12:24 +0200

    Remove console.time

--- server/authentication/authenticate.js
+++ server/authentication/authenticate.js
@@ -3,19 +3,14 @@ const UserService = require("../services/user");
 const userService = new UserService();
 const authenticate = async function (req, res, next) {
   const authHeader = req.headers.authorization;
-  console.time("authenticate");
   if (authHeader) {
     const token = authHeader.replace("animalAidAuthorization ", "");
-    console.time("authenticate3");
     let data = decryptToken(token).split(";");
-    console.timeEnd("authenticate3");
     if (data[0] !== "") {
       if (parseInt(data[1]) < parseInt(new Date().getTime() / 1000)) {
         res.sendStatus(401);
       } else {
-        console.time("authenticate2");
         const role = await userService.getRole(data[0]);
-        console.timeEnd("authenticate2");
         let user = { id: data[0], role: role };
         if (role === false) {
           res.sendStatus(401);
@@ -30,6 +25,5 @@ const authenticate = async function (req, res, next) {
   } else {
     res.sendStatus(401);
   }
-  console.timeEnd("authenticate");
 };
 module.exports = authenticate;
