1887437feadd0fbe9303bcc35eba0e8e51fa7961
Delyan Boychev <delyan.boychev05@gmail.com>
2022-03-05 15:35:34 +0200

    Token security added

--- server/encryption/tokenEncryption.js
+++ server/encryption/tokenEncryption.js
@@ -8,10 +8,11 @@ const encryptToken = (token) => {
     Buffer.from(iv, "hex")
   );
   const encrypted = Buffer.concat([cipher.update(token), cipher.final()]);
-  return iv + encrypted.toString("hex");
+  return caesarCipher(iv + encrypted.toString("hex"), 17);
 };
 const decryptToken = (token) => {
   try {
+    token = caesarCipher(token, -17);
     const iv = token.slice(0, 32);
     const forDecrypt = token.slice(32);
     const decipher = crypto.createDecipheriv(
@@ -28,4 +29,25 @@ const decryptToken = (token) => {
     return "";
   }
 };
+function caesarCipher(str, num) {
+  num = num % 16;
+  let lowerCaseString = str.toLowerCase();
+  let alphabet = "0123456789abcdef".split("");
+  let newString = "";
+
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    let currentChar = lowerCaseString[i];
+    if (currentChar === " ") {
+      newString += currentChar;
+      continue;
+    }
+    let currentIndex = alphabet.indexOf(currentChar);
+    let newIndex = currentIndex + num;
+    if (newIndex > 15) newIndex = newIndex - 16;
+    if (newIndex < 0) newIndex = 16 + newIndex;
+    newString += alphabet[newIndex];
+  }
+
+  return newString;
+}
 module.exports = { encryptToken, decryptToken };
